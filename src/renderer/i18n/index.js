a11d00848a4a7321168278fb6d8f95a8d6853e1f
greguz <gregolettogiacomo@gmail.com>
2017-09-19 10:24:42 +0200

    add vuex-i18n package

---
+++ src/renderer/i18n/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import i18n from 'vuex-i18n'
+import store from '../store'
+
+Vue.use(i18n.plugin, store)
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import store from './store'
+import './i18n'
 import App from './App'
 
 // Vue.config.productionTip = false
