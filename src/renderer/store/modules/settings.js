8d29e7ecfcc9454bfed51145936e4b4925c4fe69
greguz <gregolettogiacomo@gmail.com>
2017-09-26 14:26:45 +0200

    add loadLocale action

--- src/renderer/store/modules/settings.js
+++ src/renderer/store/modules/settings.js
@@ -1,9 +1,20 @@
+import util from 'util'
+import path from 'path'
+import fs from 'fs'
+import Vue from 'vue'
+
+/**
+ * promisification
+ */
+
+const readFile = util.promisify(fs.readFile)
+
 /**
  * Base state on store creation
  */
 
 export const state = {
-  language: 'en'
+  locale: 'en'
 }
 
 /**
@@ -22,4 +33,22 @@ export const mutations = { }
  * Async store functions
  */
 
-export const actions = { }
+export const actions = {
+
+  /**
+   * load locale translations
+   */
+
+  async loadLocale ({ commit, dispatch, getters, state }, locale) {
+    locale = locale || state.locale || 'en'
+    const translations = JSON.parse(
+      await readFile(
+        path.join(__static, 'locales', locale + '.json'),
+        'utf8'
+      )
+    )
+    Vue.i18n.replace(locale, translations)
+    Vue.i18n.set(locale)
+  }
+
+}
